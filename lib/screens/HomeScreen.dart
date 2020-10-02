a01484170ff41b6293306d634d83d22d3f9abe85
Thaer Assfour <thaer.assfour@gmail.com>
2020-10-02 11:55:36 +0300

    02-10
    02-10

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,12 +1,11 @@
 import 'dart:convert';
-import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:newsly/classes/article.dart';
 import 'package:http/http.dart' as http;
-import 'package:newsly/screens/NewsCat.dart';
+import 'package:newsly/screens/NewsCatgory.dart';
 import 'package:newsly/widgets/newsCard.dart';
 
 import '../var.dart';
@@ -28,7 +27,6 @@ class _HomeScreenState extends State<HomeScreen> {
     "Technology"
   ];
   List<Article> myArticles = [];
-
 
   Future<List<Article>> getArticleList() async {
     var url =
@@ -60,51 +58,60 @@ class _HomeScreenState extends State<HomeScreen> {
                     fontStyle: FontStyle.normal,
                     fontWeight: FontWeight.bold,
                     fontSize: 24,
+                    letterSpacing: 2,
                     decoration: TextDecoration.none,
                     color: themeColor[0])),
             centerTitle: true,
             backgroundColor: themeColor[3],
           ),
-          drawer: Drawer(),
+        //  drawer: Drawer(),
           body: Stack(children: [
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-
                 Container(
-                  height: 64,
-                  child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: newsType.length,
-                      padding: EdgeInsets.fromLTRB(0, 6, 0, 6),
-                      itemBuilder: (context, index) {
-                        return InkWell(
-                          onTap: () {
-                            Navigator.push(context, MaterialPageRoute(builder: (context){return NewsCat(newsType[index]);}));
-                        },
-                          child: Container(
-                            margin: EdgeInsets.fromLTRB(8, 0, 8, 0),
-                            alignment: Alignment.center,
-                            padding: EdgeInsets.only(
-                                left: 24, right: 24, top: 12, bottom: 12),
-                            decoration: BoxDecoration(
-                                color: themeColor[2],
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(16)),
-                                border: Border.all(
-                                    color: themeColor[3],
-                                    width: 2,
-                                    style: BorderStyle.solid)),
-                            child: Text(newsType[index],
-                                style: GoogleFonts.lato(
-                                    fontStyle: FontStyle.normal,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                    decoration: TextDecoration.none,
-                                    color: themeColor[0])),
-                          ),
-                        );
-                      }),
+                  height: MediaQuery.of(context).orientation ==
+                          Orientation.landscape
+                      ? null
+                      : 64,
+                  child: MediaQuery.of(context).orientation ==
+                          Orientation.landscape
+                      ? null
+                      : ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: newsType.length,
+                          padding: EdgeInsets.fromLTRB(0, 6, 0, 6),
+                          itemBuilder: (context, index) {
+                            return InkWell(
+                              onTap: () {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) {
+                                  return NewsCat(newsType[index]);
+                                }));
+                              },
+                              child: Container(
+                                margin: EdgeInsets.fromLTRB(8, 0, 8, 0),
+                                alignment: Alignment.center,
+                                padding: EdgeInsets.only(
+                                    left: 24, right: 24, top: 12, bottom: 12),
+                                decoration: BoxDecoration(
+                                    color: themeColor[0],
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(16)),
+                                    border: Border.all(
+                                        color: themeColor[3],
+                                        width: 1,
+                                        style: BorderStyle.solid)),
+                                child: Text(newsType[index],
+                                    style: GoogleFonts.lato(
+                                        fontStyle: FontStyle.normal,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                        decoration: TextDecoration.none,
+                                        color: themeColor[3])),
+                              ),
+                            );
+                          }),
                 ),
                 SizedBox(
                   height: 25,
@@ -113,25 +120,38 @@ class _HomeScreenState extends State<HomeScreen> {
                   padding: const EdgeInsets.only(
                     left: 16,
                   ),
-                  child: Text(
-                    "Latest News:",
-                    style: GoogleFonts.lato(
-                        fontStyle: FontStyle.normal,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 28,
-                        decoration: TextDecoration.none,
-                        color: themeColor[3]),
-                  ),
+                  child: MediaQuery.of(context).orientation ==
+                          Orientation.landscape
+                      ? null
+                      : Text(
+                          "Latest News:",
+                          style: GoogleFonts.lato(
+                              fontStyle: FontStyle.normal,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 28,
+                              decoration: TextDecoration.none,
+                              color: themeColor[3]),
+                        ),
                 ),
                 Flexible(
                   child: isLoading
                       ? Align(
                           alignment: Alignment.center,
                           child: CircularProgressIndicator())
-                      : ListView.builder(
+                      : GridView.builder(
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount:
+                                      MediaQuery.of(context).orientation ==
+                                              Orientation.landscape
+                                          ? 2
+                                          : 1,
+                              childAspectRatio: MediaQuery.of(context).orientation == Orientation.landscape
+                                  ? 25/17
+                                  : 25/22),
                           itemCount: myArticles.length,
                           itemBuilder: (context, index) {
-                            return newsCard(myArticles[index]);
+                            return newsCard(myArticles[index], context);
                           }),
                 )
               ],
